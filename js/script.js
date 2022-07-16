9bf7bc2828251c7ab303b97e942838e38511a070
Arthur <arthurschultz1@gmail.com>
2022-07-16 13:33:41 -0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,14 @@
+function media(){
+var number = parseInt(document.getElementById("number").value);
+media = (number/2);
+
+
+if (number%2 == 0) {
+    console.log ("par");
+}else {
+    console.log ("impar");
+}
+
+}
+
+
